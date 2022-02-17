9e7a71ca11a4464a41b0bfb2059ff6b5ee0aebaa
Radixrohit <rsk1200@gmail.com>
2022-02-17 13:27:04 +0530

    Added more components of function based

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import Cart from './components/Initial Days/Cart';
+
+import DropdownSelect from './components/2. Show dynamic content/DropdownSelect';
+import Conditionalview from './components/3. Conditional looping/Conditionalview';
+import ConditionOne from './components/3. Conditional looping/ConditionOne';
+import ConditionMultiple from './components/3. Conditional looping/ConditionMultiple';
 
 function App() {
   return (
-    <Cart/>
+    <ConditionMultiple/>
   );
 }
 
